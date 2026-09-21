fe61fa3303d40ee68c8b540dc93d3e36fe87de87
frindle <69526085+frindle@users.noreply.github.com>
2026-09-21 05:23:11 -0700

    slice s3-citi-typ-citi-thankyou-p: citi_typ 'Citi ThankYou Points': flying_blue, jetblue,

--- src/transfer_partners.py
+++ src/transfer_partners.py
@@ -1,26 +1,5 @@
-"""Transfer partner registry keyed by program slug."""
-
 PROGRAMS = {
-    'amex_mr': {
-        'name': 'American Express Membership Rewards',
-        'transfers_to': [
-            'hyatt_worldwide',
-            'marriott_bonvoy',
-            'hilton_honors',
-            'delta_sky_miles',
-        ],
-    },
-    'chase_ur': {
-        'name': 'Chase Ultimate Rewards',
-        'transfers_to': [
-            'air_canada',
-            'american',
-            'united',
-            'southwest',
-            'jetblue',
-            'british_airways',
-            'singapore',
-            'virgin_atlantic',
-        ],
-    },
+    "citi_typ": {"name": "Citi ThankYou Points",
+                 "transfers_to": ["flying_blue", "jetblue", "qantas", "qatar", "singapore",
+                                  "turkish", "virgin_atlantic", "etihad", "emirates"]},
 }

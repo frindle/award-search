40e8104fa836990e3346ec74c0c6bfd224b72a92
frindle <69526085+frindle@users.noreply.github.com>
2026-09-21 05:43:54 -0700

    slice s4-capital-one-capital-one: capital_one 'Capital One Miles': air_canada, emirates,

--- src/transfer_partners.py
+++ src/transfer_partners.py
@@ -2,4 +2,7 @@ PROGRAMS = {
     "citi_typ": {"name": "Citi ThankYou Points",
                  "transfers_to": ["flying_blue", "jetblue", "qantas", "qatar", "singapore",
                                   "turkish", "virgin_atlantic", "etihad", "emirates"]},
+    "capital_one": {"name": "Capital One Miles",
+                    "transfers_to": ["air_canada", "emirates", "etihad", "finnair", "flying_blue",
+                                     "qantas", "singapore", "turkish", "virgin_atlantic", "qatar"]},
 }

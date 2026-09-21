168e5521af79dc72b6a44080c597af4df394c088
frindle <69526085+frindle@users.noreply.github.com>
2026-09-21 01:38:01 -0700

    slice s1-amex-mr-american-express: amex_mr 'American Express Membership Rewards': aer_lingus->NOT

---
+++ src/transfer_partners.py
@@ -0,0 +1,13 @@
+"""Transfer partner registry keyed by program slug."""
+
+PROGRAMS = {
+    'amex_mr': {
+        'name': 'American Express Membership Rewards',
+        'transfers_to': [
+            'hyatt_worldwide',
+            'marriott_bonvoy',
+            'hilton_honors',
+            'delta_sky_miles',
+        ],
+    },
+}
